25ab013f6e304cc2d3e77de138e37c8ef8b19518
sergiosaint <sergio.n.lopes@gmail.com>
2026-06-21 14:44:01 +0100

    some fixes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/Calculador de Rentabilidade de Investimento Imobiliário/i);
+  expect(heading).toBeInTheDocument();
 });
